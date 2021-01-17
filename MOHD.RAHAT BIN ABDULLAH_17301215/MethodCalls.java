2545b9823ddae87afc303f26796133d4a122e1bf
Mohd.Rahat Bin Abdullah <59834700+Rahat-bin-abdullah-omi@users.noreply.github.com>
2021-01-17 21:46:46 +0600

    Add files via upload

---
+++ MOHD.RAHAT BIN ABDULLAH_17301215/MethodCalls.java
@@ -0,0 +1,28 @@
+
+public interface MethodCalls {
+ // method declaration for factorial.
+ public int Factorial(int x);
+ // method declaration for Fibonacci number.
+ public int Fibonacci(int y);
+ // method declaration for recursive array
+ public void RecursiveArray(int [] arr, int z);
+ //method declaration for Binary number
+ public String BinaryNumber(int b);
+ // method declaration to calculate the power of a number (m ^ n)
+ public double power(int m , int n);
+ // method declaration for non dummy headed linked linear list
+ public Object LinearListSum(Node u);
+ // side method for conversion of array to linked list
+ public Node ConvertSingly(int []b);
+ // method declaration for reversed linked list recursively
+ public void ReverseList(Node v);
+ // method declaration for recursive selection sort
+ public void recurSelectionSort(int []a , int n);
+ // method declaration for minimum index
+ public int getMinimumIndex(int[] c, int y);
+ // method declaration for Recursive insertion sort
+ public void insertionSortRecursive(int arr[], int n);
+ //
+ public void printList(Node head);
+
+}
